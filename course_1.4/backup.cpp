d8e886a3feb083dbe62f553317fb8b425f1ae3d9
Quankang Wang <Quankang.Wang@arm.com>
2017-05-26 11:33:06 +0800

    problems with vector

--- course_1.4/backup.cpp
+++ course_1.4/backup.cpp
@@ -1,7 +1,7 @@
 #include "mbed.h"
 #include <string>
 #include <vector>
-#include <inttypes.h>
+#include <inttypes.h>// seems to not in mbed OS5?
 #include "mbedtls/entropy_poll.h"
 #include <sstream>
 DigitalOut led(LED1);
@@ -29,29 +29,29 @@ void do_blink(){
         printf("blink initialized\n");
      while(1){
          //debug the position and the delay time in vector
-         printf("position is at %d\n",blinkarg->position);
-         printf("delay time is %d\n",blinkarg->blink_pattern.at(blinkarg->position));
+         //printf("position is at %u\n",(unsigned int)blinkarg->position);
+         //printf("delay time is %u\n",(unsigned int)blinkarg->blink_pattern.at(blinkarg->position));
          
          led=!led;
          if(blinkarg->position >= blinkarg->blink_pattern.size()){
-            printf("size is %d \n",blinkarg->blink_pattern.size());
+         //   printf("size is %d \n",blinkarg->blink_pattern.size());
              led=0;
              printf("finished blinking\n");
             return;
          }
-         wait(blinkarg->blink_pattern.at(blinkarg->position));
+         Thread::wait(blinkarg->blink_pattern.at(blinkarg->position));
          blinkarg->position++;
          printf("position ++\n");
      }
 }
 
 int main() {
+        blinkarg->clear();
         printf("helloworld\n");
         led=0;
         // std::string initial the pattern;
         std::string s("100:500:100:500:100:500:100:500:100");
         printf("led_execute_callback pattern=%s\n",s.c_str());
-        blinkarg->clear();
         // our pattern is something like 500:200:500, so parse that
         std::size_t found = s.find_first_of(":");
         
@@ -70,7 +70,7 @@ int main() {
             if(found == std::string::npos) {
                 blinkarg->blink_pattern.push_back(atoi((const char*)s.c_str()));
             }
-        }
-    
-       blinky_thread.start(callback(do_blink));
+        }   
+        do_blink();  
+//       blinky_thread.start(callback(do_blink));
     }
